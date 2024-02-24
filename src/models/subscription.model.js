a831977fd45de1bcdd61aacb645880c27c95856a
gurpreetsinghhh00 <gurpreetsinghhh00@gmail.com>
2024-02-24 21:40:23 +0530

    added subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+import { User } from "./user.model";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = model("Subscription", subscriptionSchema);
